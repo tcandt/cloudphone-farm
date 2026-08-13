7df2db1a3f8b373ccb0774578241f054d6dad90a
tcandt <tinhyt2018@gmail.com>
2026-08-13 19:34:08 +0700

    fix(vite): enable polling watcher in vite.config.ts for reliable HMR

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+    },
   },
 });
